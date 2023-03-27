540afbe1f7506de56db32a701989f97fb69256f6
longzhili <longzhili2005@126.com>
2023-03-27 17:45:21 +0800

    fix v0.1.1

--- push_test.sh
+++ push_test.sh
@@ -5,6 +5,7 @@ git commit -am "fix ${VERSION}"
 git push
 git tag -d ${VERSION}
 git tag ${VERSION}
+git push --tags
 
 pod cache clean ${NAME}
 pod repo push hsxorg *.podspec --use-libraries --allow-warnings --skip-import-validation --sources='https://github.com/longzl/PodSpec.git,https://github.com/CocoaPods/Specs.git'
