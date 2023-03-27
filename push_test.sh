3b964255b6ec080c61d522071c85205f335056de
longzhili <longzhili2005@126.com>
2023-03-27 17:44:32 +0800

    fix v0.1.1

---
+++ push_test.sh
@@ -0,0 +1,10 @@
+VERSION="v0.1.1"
+NAME="HSXCAPKit-sqlcipher"
+git add .
+git commit -am "fix ${VERSION}"
+git push
+git tag -d ${VERSION}
+git tag ${VERSION}
+
+pod cache clean ${NAME}
+pod repo push hsxorg *.podspec --use-libraries --allow-warnings --skip-import-validation --sources='https://github.com/longzl/PodSpec.git,https://github.com/CocoaPods/Specs.git'
