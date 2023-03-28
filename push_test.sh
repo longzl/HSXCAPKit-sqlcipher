65189bb29284a6e490aa853a77c46405c8ed2a03
longzhili <longzhili2005@126.com>
2023-03-28 15:00:04 +0800

    fix v0.1.1

--- push_test.sh
+++ push_test.sh
@@ -12,10 +12,9 @@ git push --tags
 # export LDFLAGS="-L/Users/longzl/code/git/Build-OpenSSL-cURL/archive/libcurl-7.84.0-openssl-1.1.1p-nghttp2-NONE/lib/iOS"
 # export CPPFLAGS="-I/Users/longzl/code/git/Build-OpenSSL-cURL/archive/libcurl-7.84.0-openssl-1.1.1p-nghttp2-NONE/include"
 
-export PATH=/usr/local/Cellar/openssl@1.1/1.1.1t/bin:$PATH
-openssl version -a
-export LDFLAGS="-L/usr/local/Cellar/openssl@1.1/1.1.1t/lib"
-export CPPFLAGS="-I/usr/local/Cellar/openssl@1.1/1.1.1t/include"
+# export PATH=/usr/local/Cellar/openssl@1.1/1.1.1t/bin:$PATH
+# export LDFLAGS="-L/usr/local/Cellar/openssl@1.1/1.1.1t/lib"
+# export CPPFLAGS="-I/usr/local/Cellar/openssl@1.1/1.1.1t/include"
 
 pod cache clean ${NAME}
 pod repo push hsxorg *.podspec --use-libraries --verbose --allow-warnings --skip-import-validation --sources='https://github.com/longzl/PodSpec.git,https://github.com/CocoaPods/Specs.git'
