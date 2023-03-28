7fe5e4eb6182cd9a060499001c20e93dd760f59f
longzhili <longzhili2005@126.com>
2023-03-28 11:21:58 +0800

    fix v0.1.1

--- HSXCAPKit-sqlcipher.podspec
+++ HSXCAPKit-sqlcipher.podspec
@@ -11,7 +11,7 @@ Pod::Spec.new do |s|
   s.platform     = :ios, "11.0.0"
   s.source       = { :git => "https://github.com/longzl/HSXCAPKit-sqlcipher.git", :tag => "v#{s.version}" }
   s.header_mappings_dir = "sqlcipher"
-  s.libraries     = "c++"
+  s.libraries     = "c++","openssl"
   s.source_files  = "sqlcipher/*.{h,c}"
   s.compiler_flags  = '-DSQLITE_HAS_CODEC'
 
--- push_test.sh
+++ push_test.sh
@@ -18,4 +18,4 @@ export LDFLAGS="-L/usr/local/Cellar/openssl@1.1/1.1.1t/lib"
 export CPPFLAGS="-I/usr/local/Cellar/openssl@1.1/1.1.1t/include"
 
 pod cache clean ${NAME}
-pod repo push hsxorg *.podspec --use-libraries --allow-warnings --skip-import-validation --sources='https://github.com/longzl/PodSpec.git,https://github.com/CocoaPods/Specs.git'
+pod repo push hsxorg *.podspec --use-libraries --verbose --allow-warnings --skip-import-validation --sources='https://github.com/longzl/PodSpec.git,https://github.com/CocoaPods/Specs.git'
